eb06b43e3d145268e9eb0af4476f66c0ee2361f4
Nami <128851111+WilliamMarquesJr@users.noreply.github.com>
2025-11-29 17:54:00 -0300

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ client.once("ready", () => {
     cron.schedule("0 12 * * *", async () => {
         try {
             const channel = await client.channels.fetch(CHANNEL_ID);
-            if (channel) channel.send("Eu te amo ❤️");
+            if (channel) channel.send("Neném eu te amo, você é a minha fofinha cuti cuti.");
         } catch (error) {
             console.error("Erro ao enviar mensagem das 12h:", error);
         }
@@ -29,9 +29,19 @@ client.once("ready", () => {
     cron.schedule("40 15 * * *", async () => {
         try {
             const channel = await client.channels.fetch(CHANNEL_ID);
-            if (channel) channel.send("Eu te amo ❤️");
+            if (channel) channel.send("Princesa linda, flor do meu jardim do amor. Eu te amo.");
         } catch (error) {
             console.error("Erro ao enviar mensagem das 15:40:", error);
+        }
+    });
+
+    // Enviar às 19:00
+    cron.schedule("0 19 * * *", async () => {
+        try {
+            const channel = await client.channels.fetch(CHANNEL_ID);
+            if (channel) channel.send("Bunda gostosa da minha vida, eu te amo.");
+        } catch (error) {
+            console.error("Erro ao enviar mensagem das 19h:", error);
         }
     });
 });
