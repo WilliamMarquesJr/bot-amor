6be655596b31ef013b800db27941a2d8a03e84a9
Nami Cinna <128851111+WilliamMarquesJr@users.noreply.github.com>
2025-11-29 18:16:37 -0300

    Update index.js

--- index.js
+++ index.js
@@ -46,4 +46,13 @@ client.once("ready", () => {
     });
 });
 
+// ====== Comando: /amor ======
+client.on("interactionCreate", async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === "amor") {
+        await interaction.reply("Minha princesa linda, eu te amo ❤️");
+    }
+});
+
 client.login(TOKEN);
